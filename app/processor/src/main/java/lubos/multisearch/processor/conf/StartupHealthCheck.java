ce4c52651738e9fd7ec4cbf34fe68bf397f281bf
Lubos <alexvas06@gmail.com>
2025-05-28 00:22:06 +0300

    Added simple healthcheck for container.

---
+++ app/processor/src/main/java/lubos/multisearch/processor/conf/StartupHealthCheck.java
@@ -0,0 +1,17 @@
+package lubos.multisearch.processor.conf;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
+@Component
+public class StartupHealthCheck implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        File healthCheckFile = new File("/tmp/healthy");
+        healthCheckFile.createNewFile();
+        Runtime.getRuntime().addShutdownHook(new Thread(healthCheckFile::delete));
+    }
+}
