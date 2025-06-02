68f5815da7e1d354be2a3e85babe6582e71dba18
Lubos <alexvas06@gmail.com>
2025-06-02 06:16:32 +0300

    Removed AUTH_PATH, added MONGO_PORT, RABBIT_PORT, RABBIT_MANAGEMENT_PORT compose env vars

--- conf/build/buildMockCertificates.sh
+++ conf/build/buildMockCertificates.sh
@@ -2,10 +2,10 @@
 
 #mkdir -p ca rabbit/{app,server} mongo/{app,server} elastic/{app,fleet,agent,kibana,server}
 #mkdir -p ca rabbit/app rabbit/server mongo/app mongo/server elastic/app elastic/fleet elastic/agent elastic/kibana elastic/server
-mkdir testCert
+mkdir certs
 echo "Generating Build Phase Mock CA certificate"
-openssl genrsa -out testCert/ca.key 4096
-openssl req -x509 -new -key testCert/ca.key --days 1024 -out testCert/ca.pem -section ca -config cert_conf/buildOpenssl.conf
+openssl genrsa -out certs/ca.key 4096
+openssl req -x509 -new -key certs/ca.key --days 1024 -out certs/ca.pem -section ca -config cert_conf/buildOpenssl.conf
 echo "Test CA generated successfully at ${PWD}"
 
 create_certificate()
@@ -13,14 +13,14 @@ create_certificate()
   DESTINATION=$1
   SECTION=$2
   echo "Generating Build Phase Mock certificate for $SECTION"
-  openssl req -x509 -days 365 -noenc -out $DESTINATION.pem -keyout $DESTINATION.key -section $SECTION -config cert_conf/buildOpenssl.conf -CA testCert/ca.pem -CAkey testCert/ca.key
+  openssl req -x509 -days 365 -noenc -out $DESTINATION.pem -keyout $DESTINATION.key -section $SECTION -config cert_conf/buildOpenssl.conf -CA certs/ca.pem -CAkey certs/ca.key
   echo "Generated successfully at ${PWD}/$DESTINATION"
 }
 
-create_certificate testCert/tgAppRabbit rabbit_client
-create_certificate testCert/tgAppMongo mongo_client
-#create_certificate testCert/tgAppElastic elastic_client
+create_certificate certs/tgAppRabbit rabbit_client
+create_certificate certs/tgAppMongo mongo_client
+#create_certificate certs/tgAppElastic elastic_client
 
 #chown -R root:root .;
-find testCert -type d -exec chmod 755 {} \;
-find testCert -type f \( -name "*.pem" -o -name "*.key" \) -exec chmod 644 {} \;
+find certs -type d -exec chmod 755 {} \;
+find certs -type f \( -name "*.pem" -o -name "*.key" \) -exec chmod 644 {} \;
